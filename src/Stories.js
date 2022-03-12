c1c9a363d19600f4a7b713d74223e7fa29b13d56
Romário Moreira <romario.ti@hotmail.com>
2022-03-12 02:35:58 -0300

    criação dos componentes Stories

--- src/Stories.js
+++ src/Stories.js
@@ -1,57 +1,57 @@
+import Storie from "./Storie"
+
 export default function Stories() {
     return (
         <div class="stories">
-            <div class="storie">
-                <div class="conjunto">
-                    <img class="storie-imagem" src="./imagens/9gag 1.svg" alt="9gag" />
-                </div>
-                <small>9gag</small>
-            </div>
-            <div class="storie">
-                <div class="conjunto">
-                    <img class="storie-imagem" src="./imagens/meowed 1.svg" alt="meowed" />
-                </div>
-                <small>meowed</small>
-            </div>
-            <div class="storie">
-                <div class="conjunto">
-                    <img class="storie-imagem" src="./imagens/barked 1.svg" alt="barked" />
-                </div>
-                <small>barked</small>
-            </div>
-            <div class="storie">
-                <div class="conjunto">
-                    <img class="storie-imagem" src="./imagens/nathanwpylestrangeplanet 1.svg" alt="nathanwpyle" />
-                </div>
-                <small>nathanwpyle</small>
-            </div>
-            <div class="storie">
-                <div class="conjunto">
-                    <img class="storie-imagem" src="./imagens/wawawiwacomicsa 1.svg" alt="wawawiwacomicsa" />
-                </div>
-                <small>wawawiwac</small>
-            </div>
-            <div class="storie">
-                <div class="conjunto">
-                    <img class="storie-imagem" src="./imagens/respondeai 1.svg" alt="respondeai" />
-                </div>
-                <small>respondeai</small>
-            </div>
-            <div class="storie">
-                <div class="conjunto">
-                    <img class="storie-imagem" src="./imagens/filomoderna 1.svg" alt="filomoderna" />
-                </div>
-                <small>filomoderna</small>
-            </div>
-            <div class="storie">
-                <div class="conjunto">
-                    <img class="storie-imagem" src="./imagens/9gag 1.svg" alt="9gag" />
-                </div>
-                <small>memeriago.</small>
-            </div>
+            {stories.map(storie => {
+                return Storie(storie)
+            })}
             <div class="setinha">
                 <img src="./imagens/chevron-forward-circle 1.svg" alt="chevron-forward-circle" />
             </div>
         </div>
     )
 }
+
+const stories = [
+    {
+        imagem: "./imagens/9gag 1.svg",
+        textoAlternativo: "9gag",
+        descricao: "9gag"
+    },
+    {
+        imagem: "./imagens/meowed 1.svg",
+        textoAlternativo: "meowed",
+        descricao: "meowed"
+    },
+    {
+        imagem: "./imagens/barked 1.svg",
+        textoAlternativo: "barked",
+        descricao: "barked"
+    },
+    {
+        imagem: "./imagens/nathanwpylestrangeplanet 1.svg",
+        textoAlternativo: "nathanwpyle",
+        descricao: "nathanwpyle"
+    },
+    {
+        imagem: "./imagens/wawawiwacomicsa 1.svg",
+        textoAlternativo: "wawawiwacomicsa",
+        descricao: "wawawiwacomicsa"
+    },
+    {
+        imagem: "./imagens/respondeai 1.svg",
+        textoAlternativo: "respondeai",
+        descricao: "respondeai"
+    },
+    {
+        imagem: "./imagens/filomoderna 1.svg",
+        textoAlternativo: "filomoderna",
+        descricao: "filomoderna"
+    },
+    {
+        imagem: "./imagens/9gag 1.svg",
+        textoAlternativo: "9gag",
+        descricao: "9gag"
+    }
+]
